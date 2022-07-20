de2e6232c24433a7e12af2229af160ed8bf7e318
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-20 17:36:16 +0200

    add statement for NaN & refactoring

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -1,23 +1,30 @@
 // chiedi all’utente il numero di chilometri che vuole percorrere e l’età del passeggero
 const chilometri = parseInt( prompt('Inserisci il numero di chilometri che vuoi percorrere.') );
 const eta = parseInt( prompt('Inserisci la tua età.') );
-console.log(chilometri, eta);
 
-// calcola il prezzo totale del viaggio sapendo che:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const prezzoBiglietto = chilometri * 0.21;
-// va applicato uno sconto del 20% per i minorenni
+let titolo = document.getElementById('titolo');
+
+const prezzoPerChilometro = 0.21;
 const scontoMinorenni = 0.2;
-const prezzoBigliettoMinorenni = prezzoBiglietto * (1 - scontoMinorenni);
-// va applicato uno sconto del 40% per gli over 65.
 const scontoOver65 = 0.4;
-const prezzoBigliettoOver65 = prezzoBiglietto * (1 - scontoOver65);
-console.log(prezzoBiglietto, prezzoBigliettoMinorenni, prezzoBigliettoOver65);
 
-if (eta < 18) {
-    document.getElementById('titolo').innerHTML = 'Il tuo biglietto costa ' + prezzoBigliettoMinorenni.toFixed(2) + '€';
-} else if (eta > 65) {
-    document.getElementById('titolo').innerHTML = 'Il tuo biglietto costa ' + prezzoBigliettoOver65.toFixed(2) + '€';
+if ( isNaN(chilometri) || isNaN(eta) ) {
+    titolo.innerHTML = 'I dati inseriti non sono corretti';
 } else {
-    document.getElementById('titolo').innerHTML = 'Il tuo biglietto costa ' + prezzoBiglietto.toFixed(2) + '€';
+    // calcola il prezzo totale del viaggio sapendo che:
+    // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    const prezzoBigliettoBase = chilometri * prezzoPerChilometro;
+    let prezzoBiglietto;
+    
+    if (eta < 18) {
+        // va applicato uno sconto del 20% per i minorenni
+        prezzoBiglietto = prezzoBigliettoBase * (1 - scontoMinorenni);
+    } else if (eta > 65) {
+        // va applicato uno sconto del 40% per gli over 65.
+        prezzoBiglietto = prezzoBigliettoBase * (1 - scontoOver65);
+    } else {
+        prezzoBiglietto = prezzoBigliettoBase;
+    }
+
+    titolo.innerHTML = 'Il tuo biglietto costa ' + prezzoBiglietto.toFixed(2) + ' €';
 }
